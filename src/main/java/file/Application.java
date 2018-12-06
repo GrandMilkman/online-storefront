c3459bcaa4ef9bee92f68e34ab23c542df6f8f71
root <root@grandmilkman-Inspiron-5570>
2018-12-06 23:11:35 +0300

    fix (and it fcng works! at least we can run it on server now)

--- src/main/java/file/Application.java
+++ src/main/java/file/Application.java
@@ -5,8 +5,20 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 @ComponentScan
-public class Application {
-
+public class Application /*implements WebApplicationInitializer*/{
+    
+    /*@Override
+    public void onStartup(ServletContext servletContext) throws ServletException {
+        
+        XmlWebApplicationContext context = new XmlWebApplicationContext();
+        context.setConfigLocation("/WEB-INF/mvc-servlet.xml");
+        
+        ServletRegistration.Dynamic mvc = servletContext.addServlet("mvc", new DispatcherServlet(context));
+        mvc.setLoadOnStartup(1);
+        mvc.addMapping("/storefront");
+        
+    }
+    */
     public static void main(String[] args) {
         final ConfigurableApplicationContext ctx = new ClassPathXmlApplicationContext("classpath:config.xml");
         ctx.start();
