60ff7b1ebd9c88a130bfe98d72acde57472f0bdb
PiArtit <vic@stellarium>
2018-12-10 22:30:19 +0300

    connect to bd

--- src/main/java/file/mvc/JSPIndexController.java
+++ src/main/java/file/mvc/JSPIndexController.java
@@ -1,13 +1,14 @@
 package file.mvc;
 
 import java.io.IOException;
-
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+
 /*
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -30,8 +31,11 @@ public class IndexController {
 @WebServlet(name = "IndexServlet", urlPatterns = "/index")
 public class JSPIndexController extends HttpServlet {
 
+
 	private void processRequest(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
+
+		
 
 		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/index.jsp");
 		dispatcher.forward(request, response);
--- src/main/java/file/mvc/JSPUserListController.java
+++ src/main/java/file/mvc/JSPUserListController.java
@@ -9,6 +9,9 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import file.entity.User;
+import file.service.impl.UserServiceImpl;
+
 @WebServlet(name = "UserListServlet", urlPatterns = "/user_list")
 public class JSPUserListController extends HttpServlet {
 
@@ -17,13 +20,39 @@ public class JSPUserListController extends HttpServlet {
 
 		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/user_list.jsp");
 		dispatcher.forward(request, response);
+
 	}
+
+	private UserServiceImpl userServiceImpl = new UserServiceImpl();
 
 	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 
-		processRequest(request, response);
+		Long id = (long) 0;
+		request.setCharacterEncoding("UTF-8");
+		User USER = userServiceImpl.getUser(id);
+		String name = USER.getName();
+
+		/*
+		 * String login = request.getParameter("login"); String pass =
+		 * request.getParameter("pass"); RegistrationHelper registrationHelper = new
+		 * RegistrationHelper(); registrationHelper.setLogin(login);
+		 * 
+		 * boolean isError = false; if (login == null || login.trim().equals("")) {
+		 * String loginMessage = "It's empy";
+		 * registrationHelper.setLoginMessage(loginMessage); isError = true; } if (pass
+		 * == null || pass.trim().equals("")) { String passMessage = "It's empty.";
+		 * registrationHelper.setPassMessage(passMessage); isError = true; }
+		 * if(!isError) {
+		 * 
+		 * 
+		 * 
+		 * UsersDAO dao = (UsersDAO)
+		 * request.getServletContext().getAttribute("usersDAO");
+		 */
+		request.setAttribute("USER", name); // Will be available as ${product} in JSP
+		request.getRequestDispatcher("/WEB-INF/product.jsp").forward(request, response);
 	}
 
 	@Override
@@ -32,5 +61,5 @@ public class JSPUserListController extends HttpServlet {
 
 		processRequest(request, response);
 	}
+
 }
- 
